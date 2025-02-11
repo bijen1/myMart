7f6ebcc6c3b62adca6654d80e32da5f6e53f6173
Bijendra Sharma <bijendrasharma@Bijendras-MacBook-Air.local>
2025-02-11 20:03:29 +0530

    Initial commit

---
+++ src/main/java/com/mymart/mymart/MymartApplication.java
@@ -0,0 +1,13 @@
+package com.mymart.mymart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mymart/mymart/MymartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymart.mymart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
